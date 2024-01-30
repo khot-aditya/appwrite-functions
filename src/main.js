1753849c64a6c0b012a5e53a4806a75f622466e9
khot-aditya <adityakhot6838@gmail.com>
2024-01-30 21:29:36 +0530

    added POST method

--- src/main.js
+++ src/main.js
@@ -1,26 +1,20 @@
 import { Client } from 'node-appwrite';
+import { spawn } from "child_process"
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
   if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
     return res.send('Hello, World!');
+  }
+
+  if (req.method === 'POST') {
+    
+    const username = req.query.username;
+    const password = req.query.password;
+    const chatID = req.query.chatID;
+    const message = req.query.message;
+    
+    return res.send(`Hello, World! ${username}${password}${chatID}${message}`);
   }
 
   // `res.json()` is a handy helper for sending JSON
